0de3927cdab75a7de7171510fffac7f648f3332f
aagneye-syam <saagneye2003@gmail.com>
2024-05-18 22:44:12 +0530

    struct defined in CrowdFunding.sol

---
+++ web3/contracts/CrowdFunding.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+contract CrowdFunding {
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donators;
+    }
+}
