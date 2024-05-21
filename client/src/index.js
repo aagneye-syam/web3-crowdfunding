b0ed2281e7b3807a66aee1daafff97f0b1abdab6
aagneye-syam <saagneye2003@gmail.com>
2024-05-21 09:08:31 +0530

    necessary pkg imported

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react'
+
+
