8bb17c37016ab65cc28006a4fbed29ab4a3b0759
aagneye-syam <saagneye2003@gmail.com>
2024-05-24 18:06:40 +0530

    index.css connected

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import ReactDom from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react"; // Removed extra space
 import App from "./App";
+import "./index.css";
 const root = ReactDom.createRoot(document.getElementById("root"));
 
 root.render(
