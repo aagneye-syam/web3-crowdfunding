e8248efcff1db1ba53b75c1698bf6cb5aaea04f7
aagneye-syam <saagneye2003@gmail.com>
2024-05-26 19:18:07 +0530

    tailwind class for sidebar and navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Route,Routes} from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <h1>APP</h1>
+    <div className="relative sm:-8 p-4 bg-[#1313a] min-h-screen flex flex-row" >
+      <div className="sm:flex hidden mr-10 relative">
+        Sidebar
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        Navbar
+
+        <
+      </div>
     </div>
   )
 }
