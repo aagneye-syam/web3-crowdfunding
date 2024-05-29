df1cc33e65aac06ff52ebb2565c9f49743694e1a
aagneye-syam <saagneye2003@gmail.com>
2024-05-29 19:59:11 +0530

    created Profile,Home,CreateCampaign,CreateDetails pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,9 @@ const App = () => {
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         Navbar
 
-        <
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
       </div>
     </div>
   )
