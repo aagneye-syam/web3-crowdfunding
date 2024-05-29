9521881eee9a6e84df8a13f80637643a3772b7c7
aagneye-syam <saagneye2003@gmail.com>
2024-05-29 20:25:09 +0530

    Navbar and Sidebar added

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default navbar
---
+++ client/src/components/SIdebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SIdebar = () => {
+  return (
+    <div>
+      navbar
+    </div>
+  )
+}
+
+export default SIdebar
