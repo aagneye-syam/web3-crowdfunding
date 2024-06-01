e84f126693012162c35f854b30503e04438055b7
aagneye-syam <saagneye2003@gmail.com>
2024-06-01 23:30:51 +0530

    Link component added

--- client/src/components/SIdebar.jsx
+++ client/src/components/SIdebar.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react'
-import {Link, useNavigate} from 'react-router-dom';
-import {dashboard, logo, sun} from '../assets';
-import {navlinks} from '../constants';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { dashboard, logo, sun } from "../assets";
+import { navlinks } from "../constants";
 
 const Sidebar = () => {
   const navigate = useNavigate();
-  const [isActive, setIsActive] = useState('dashboard');
+  const [isActive, setIsActive] = useState("dashboard");
 
   return (
-    <div>
-      Sidebar
+    <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
+      <Link to="/">
+        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
