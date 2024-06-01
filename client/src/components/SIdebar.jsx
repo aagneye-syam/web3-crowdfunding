beb690d608d6d8d198c30800e37370f7762ac0ed
aagneye-syam <saagneye2003@gmail.com>
2024-06-01 23:33:54 +0530

    active status check in Icon

--- client/src/components/SIdebar.jsx
+++ client/src/components/SIdebar.jsx
@@ -3,6 +3,27 @@ import { Link, useNavigate } from "react-router-dom";
 import { dashboard, logo, sun } from "../assets";
 import { navlinks } from "../constants";
 
+const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
+  <div
+    className={`w-[48px] h-[48px] rounded-[10px] ${
+      isActive && isActive === name && "bg-[#2c2f32]"
+    } flex justify-center items-center ${
+      !disabled && "cursor-pointer"
+    } ${styles}`}
+    onClick={handleClick}
+  >
+    {!isActive ? (
+      <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
+    ) : (
+      <img
+        src={imgUrl}
+        alt="fund_logo"
+        className={`w-1/2 h-1/2 ${isActive !== name && "grayscale"}`}
+      />
+    )}
+  </div>
+);
+
 const Sidebar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState("dashboard");
