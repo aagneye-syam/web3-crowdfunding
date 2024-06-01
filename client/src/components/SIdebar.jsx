9884262753f7a242bd00d676faa7ab4fa64cf1bf
aagneye-syam <saagneye2003@gmail.com>
2024-06-01 23:26:47 +0530

    useState created for dashboard

--- client/src/components/SIdebar.jsx
+++ client/src/components/SIdebar.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom';
+import {dashboard, logo, sun} from '../assets';
+import {navlinks} from '../constants';
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState('dashboard');
+
   return (
     <div>
       Sidebar
