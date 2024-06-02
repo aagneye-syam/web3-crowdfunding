afbdca3ffba2a054ba25834d11797d1c11cbfb3f
aagneye-syam <saagneye2003@gmail.com>
2024-06-02 21:20:05 +0530

    Loading symbol added and h1 added

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
+
+import money from "../assets";
+import { CustomButton } from "../components";
+import { checkIfImage } from "../utils";
 
 const CreateCampaign = () => {
-  return (
-    <div>
-      CreateCampaign
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const [Loading, isLoading] = useState(false);
+  const [form, setform] = useState({
+    name: "",
+    title: "",
+    description: "",
+    target: "",
+    deadline: "",
+    image: "",
+  });
 
-export default CreateCampaign
+  return (
+    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+      {isLoading && 'Loader..'}
+      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
+        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">
+          Start a Campaign
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default CreateCampaign;
