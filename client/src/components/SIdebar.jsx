d6e18c3ece7477e2a43adc1dce1b511c915e5c6e
aagneye-syam <saagneye2003@gmail.com>
2024-06-03 18:36:26 +0530

    new logo added

--- client/src/components/SIdebar.jsx
+++ client/src/components/SIdebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { dashboard, logo, sun } from "../assets";
+import { dashboard, logo01, sun } from "../assets";
 import { navlinks } from "../constants";
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
@@ -31,7 +31,7 @@ const Sidebar = () => {
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
       <Link to="/">
-        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo01} />
       </Link>
 
       <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
