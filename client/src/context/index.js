3795e4486ef8add1221dd11d69d6f1a23ef928ef
aagneye-syam <saagneye2003@gmail.com>
2024-06-04 09:05:39 +0530

    index for web3 contract handling added

---
+++ client/src/context/index.js
@@ -0,0 +1,10 @@
+import React, { useContext, createContext } from "react";
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+
+
