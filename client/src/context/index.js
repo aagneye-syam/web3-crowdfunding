54db279c1e7148afe4e14199f699a17b05b910b3
aagneye-syam <saagneye2003@gmail.com>
2024-06-04 09:06:44 +0530

    on context/index.js -> publishCampaign() added

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -7,4 +7,37 @@ import {
 } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
 
+const StateContext = createContext();
 
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract("0xabc"); //use the address from thirdweb
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign({
+        args: [
+          address, // owner
+          form.title, // title
+          form.description, // description
+          form.target,
+          new Date(form.deadline).getTime(), // deadline,
+          form.image,
+        ],
+      });
+
+      console.log("contract call success", data);
+    } catch (error) {
+      console.log("contract call failure", error);
+    }
+  };
+
+
+  
+};
