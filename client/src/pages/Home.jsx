f86f87566abf19a0b5a810e97958b6e15925e83e
aagneye-syam <saagneye2003@gmail.com>
2024-06-07 22:43:23 +0530

    on Home.jsx fetchCampaign used

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react';
+import {useStateContext} from '../context';
+
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaign, setCampaign] = useState([]);
+
+  const {address, contract, getCampaign} = useStateContext();
+
+  const fetchCampaigns = async() => {
+    setIsLoading(true);
+    const data = await getCampaign();
+    setCampaigns(data);
+    isLoading(false);
+  }
+
+  useEffect(() => {
+    if(contract)
+      fetchCampaigns
+  },[address,contract]);
+
   return (
     <div>
-      HOme
+    
     </div>
   )
 }
