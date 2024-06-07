b724bf4a993c1b7ffefa7d82dc65ce89375d813f
aagneye-syam <saagneye2003@gmail.com>
2024-06-07 23:15:15 +0530

    on CampaignDetails fetchDonators() defined

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+import { ethers } from 'ethers';
+
+import { useStateContext } from '../context';
+import { CountBox, CustomButton, Loader } from '../components';
+import { calculateBarPercentage, daysLeft } from '../utils';
+import { thirdweb } from '../assets';
+
+
 
 const CampaignDetails = () => {
+  const { state } = useLocation();
+  const navigate = useNavigate();
+  const { donate, getDonations, contract, address } = useStateContext();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [amount, setAmount] = useState('');
+  const [donators, setDonators] = useState([]);
+
+  const remainingDays = daysLeft(state.deadline);
+
+  const fetchDonators = async () => {
+    const data = await getDonations(state.pId);
+
+    setDonators(data);
+  }
+
+  useEffect(() => {
+    if(contract) fetchDonators();
+  }, [contract, address])
+
+
+
   return (
     <div>
       CampaignDetails
