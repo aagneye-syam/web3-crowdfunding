ed8ebdfe725c112fbed61211848cd196f83ae838
aagneye-syam <saagneye2003@gmail.com>
2024-06-07 23:24:44 +0530

    campaign details description added

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -49,10 +49,32 @@ const CampaignDetails = () => {
         </div>
       </div>
 
+      <div className="mt-[60px] flex lg:flex-row flex-col gap-5">
+        <div className="flex-[2] flex flex-col gap-[40px]">
+          <div>
+            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Creator</h4>
 
+            <div className="mt-[20px] flex flex-row items-center flex-wrap gap-[14px]">
+              <div className="w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#2c2f32] cursor-pointer">
+                <img src={logo01} alt="user" className="w-[60%] h-[60%] object-contain"/>
+              </div>
+              <div>
+                <h4 className="font-epilogue font-semibold text-[14px] text-white break-all">{state.owner}</h4>
+                <p className="mt-[4px] font-epilogue font-normal text-[12px] text-[#808191]">10 Campaigns</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Story</h4>
 
-      
-    </div>
+              <div className="mt-[20px]">
+                <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">{state.description}</p>
+              </div>
+          </div>
+        </div>
+       </div>
+
+  </div>
     
   );
 };
