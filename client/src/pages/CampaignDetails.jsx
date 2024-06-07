923f669bb6c04f8869127ce70175bb766faf1894
aagneye-syam <saagneye2003@gmail.com>
2024-06-07 23:18:52 +0530

    srcimg aligned

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -31,11 +31,25 @@ const CampaignDetails = () => {
   }, [contract, address])
 
 
-
   return (
     <div>
-      CampaignDetails
+      {isLoading && <Loader />}
+      <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
+        <div className="flex-1 flex-col">
+          <img src={state.image} alt="campaign" className="w-full h-[410px] object-cover rounded-xl"/>
+          <div className="relative w-full h-[5px] bg-[#3a3a43] mt-2">
+            <div className="absolute h-full bg-[#4acd8d]" style={{ width: `${calculateBarPercentage(state.target, state.amountCollected)}%`, maxWidth: '100%'}}>
+            </div>
+          </div>
+        </div>
+       
+      </div>
+
+
+
+
     </div>
+    
   );
 };
 
